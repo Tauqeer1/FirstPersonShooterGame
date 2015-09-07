6a9cfb346b25d0c214f31d7f037598780e28662f
Tauqeer1 <tauqeer.shakir@yahoo.com>
2015-09-07 15:14:33 -0700

    changes

---
+++ OculusRift_FPS/Assets/Scripts/PlayerMovementMecannim.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementMecannim : MonoBehaviour {
+
+
+    public GameObject cameraObject;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(new Vector3(0, cameraObject.GetComponent<MouseLook>().currentYRotation, 0));
+	}
+}
