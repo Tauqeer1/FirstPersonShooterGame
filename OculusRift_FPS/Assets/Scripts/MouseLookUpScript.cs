325864ba229c38160f8c7a2d638c7f4bf97eb608
tauqeer <tauqeer.shakir@yahoo.com>
2015-09-10 11:06:54 +0500

    Movement using Character controller implemented

---
+++ OculusRift_FPS/Assets/Scripts/MouseLookUpScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLookUpScript : MonoBehaviour {
+
+    public float lookSensitivity = 5.0f;
+    public float yRotation;
+    public float xRotation;
+    public float currentXRotation;
+    public float currentYRotation;
+    public float yRotationVelocity;
+    public float xRotationVelocity;
+    public float smoothTime = 0.1f;
+    public float yMinimumAngle = -60f;
+    public float yMaximumAngle = 70f;
+    
+
+
+	// Use this for initialization
+	void Start () {
+       
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        ////increase in y is the x rotation
+        //xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        ////increase in x is the y rotation
+        //yRotation += Input.GetAxis("Mouse X") * lookSensitivity;
+
+        //xRotation = Mathf.Clamp(xRotation, yMinimumAngle, yMaximumAngle);
+        
+
+        ////Smooth move in x rotation
+        //currentXRotation = Mathf.SmoothDamp(currentXRotation, xRotation, ref xRotationVelocity, smoothTime);
+        
+        ////Smooth move in y rotation
+        //currentYRotation = Mathf.SmoothDamp(currentYRotation, yRotation, ref yRotationVelocity, smoothTime);
+
+      
+
+        ////Transform the rotation of the camera
+        ////transform.rotation = Quaternion.Euler(new Vector3(xRotation, yRotation, 0));
+        //transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * lookSensitivity, Input.GetAxis("Mouse X") * lookSensitivity, 0));
+	}
+}
---
+++ OculusRift_FPS/Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementScript : MonoBehaviour {
+
+    CharacterController controller;
+    Camera characterCamera;
+    Vector3 move = Vector3.zero;
+    public float moveSpeed = 3f;
+    public float turnSpeed = 3f;
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        characterCamera = GetComponentInChildren<Camera>();
+    }
+    void Update()
+    {
+
+        //Rotation of Character
+        CharacterRotation();
+
+        ///Movement of Character
+        CharacterMovement();
+    }
+    /// <summary>
+    /// Character Rotation Function
+    /// </summary>
+    void CharacterRotation()
+    {
+        characterCamera.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y") * turnSpeed, 0, 0));
+        transform.Rotate(new Vector3(0f, Input.GetAxis("Mouse X") * turnSpeed * Time.deltaTime, 0f));
+    }
+    /// <summary>
+    /// Character Movement Function
+    /// </summary>
+    void CharacterMovement()
+    {
+        //This move the character w.r.t local space
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        //normalize the move vector
+        move.Normalize();
+
+        //TransformDirection converts from local space to world space
+        move = transform.TransformDirection(move);
+
+        //controller will move the character with the give moveSpeed
+        controller.SimpleMove(move * moveSpeed);
+    }
+
+
+    
+
+    
+}
